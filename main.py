7665bce4ca3595ee7e0edbebe4ebd2347c2afa30
numat <numat@stu.otc.school.nz>
2022-05-06 09:16:07 +1200

    setup

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 from tkinter import *  # tkinter lib
-import random # random lib
-import os # windows cmd commands lib
+import random  # random lib
+import os  # windows cmd commands lib
 
 ### UI Theme Colours
-BaseBG  = "grey16"
-BG      = "grey30"
-FG      = "grey80"
-FONT    = "Calibri"
+BaseBG = "grey16"
+BG = "grey30"
+FG = "grey80"
+FONT = "Calibri"
 ##‽TREY‽NUMA‽##
 
 root = Tk()
-root.title("Roster") # window title
-root.resizable(False, False) # disable window resizing
-root.geometry("1280x720") # window size (3:4 aspect-ratio [3]x240,[4]x240)
-root.configure(bg=BaseBG) # background colour
+frame = Frame(root)
+frame.pack()
+root.title("Roster")  # window title
+root.resizable(False, False)  # disable window resizing
+root.geometry("1280x720")  # window size
+root.configure(bg=BaseBG)  # background colour
+
+topframe = Frame(root); topframe.pack(side=TOP)
+middleframe = Frame(root); middleframe.pack(side=TOP)
+bottomframe = Frame(root); bottomframe.pack(side=TOP)
+endframe = Frame(root); endframe.pack(side=BOTTOM)
+
+
+text = Label(topframe, text="top", font=(FONT, 48, "bold"), fg=FG, bg=BG).pack()
+
+text = Label(middleframe, text="middle", font=(FONT, 24, "bold"), fg=FG, bg=BG).pack(side=LEFT)
+Open = Entry(middleframe, fg=FG, bg=BG).pack(side=RIGHT)
+
+text = Label(bottomframe, text="bottom", font=(FONT, 24, "bold"), fg=FG, bg=BG).pack()
+
+text = Label(endframe, text="end", font=(FONT, 24, "bold"), fg=FG, bg=BG).pack()
 
 
 
@@ -24,6 +41,4 @@ root.configure(bg=BaseBG) # background colour
 
 
 
-
-
-root.mainloop() # end of tk
+root.mainloop()  # end of tk
