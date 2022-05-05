d98fb64506b259cb5f28ccdf39e8c22e8d02e102
numat <numat@stu.otc.school.nz>
2022-05-06 08:58:33 +1200

    setup

---
+++ main.py
@@ -0,0 +1,29 @@
+from tkinter import *  # tkinter lib
+import random # random lib
+import os # windows cmd commands lib
+
+### UI Theme Colours
+BaseBG  = "grey16"
+BG      = "grey30"
+FG      = "grey80"
+FONT    = "Calibri"
+##‽TREY‽NUMA‽##
+
+root = Tk()
+root.title("Roster") # window title
+root.resizable(False, False) # disable window resizing
+root.geometry("1280x720") # window size (3:4 aspect-ratio [3]x240,[4]x240)
+root.configure(bg=BaseBG) # background colour
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop() # end of tk
