26e45268849698fe569ea8238bc6fd0ef5d746c2
numat <numat@stu.otc.school.nz>
2022-05-17 10:58:47 +1200

    testing out day layout

--- main.py
+++ main.py
@@ -1,36 +1,51 @@
-from tkinter import *  # tkinter lib
+from tkinter import *
+from tkinter import Canvas, Frame
 import random  # random lib
 import os  # windows cmd commands lib
+##-\‽/\TREY/\‽/-##
+
 
 ### UI Theme Colours
-BaseBG = "grey16"
-BG = "grey30"
-FG = "grey80"
-FONT = "Calibri"
-##‽TREY‽NUMA‽##
+BaseBG = ("grey16",)
+BG = ("grey30",)
+FG = ("grey80",)
+FONT = ("Calibri")
+Theme=0
+
 
 root = Tk()
 frame = Frame(root)
+canvas = Canvas()
 frame.pack()
 root.title("Roster")  # window title
 root.resizable(False, False)  # disable window resizing
 root.geometry("1280x720")  # window size
 root.configure(bg=BaseBG)  # background colour
 
-topframe = Frame(root); topframe.pack(side=TOP)
-middleframe = Frame(root); middleframe.pack(side=TOP)
-bottomframe = Frame(root); bottomframe.pack(side=TOP)
+frame1 = Frame(root);   frame1.pack(side=TOP)
+frame2 = Frame(root);   frame2.pack(side=TOP)
 endframe = Frame(root); endframe.pack(side=BOTTOM)
 
 
-text = Label(topframe, text="top", font=(FONT, 48, "bold"), fg=FG, bg=BG).pack()
+Label(frame1, text="ROSTER", font=(FONT, 48, "bold"), fg=FG, bg=BG).pack()
 
-text = Label(middleframe, text="middle", font=(FONT, 24, "bold"), fg=FG, bg=BG).pack(side=LEFT)
-Open = Entry(middleframe, fg=FG, bg=BG).pack(side=RIGHT)
+#canvas.create_rectangle(0,50,1000,110,fill=FG)
+#canvas.pack()
 
-text = Label(bottomframe, text="bottom", font=(FONT, 24, "bold"), fg=FG, bg=BG).pack()
+DaySpacing=0.135; DayFontSize=20; DayFG=FG; DayBG=BG; GridCenter=0.52; DayAnchor=CENTER
+banner = Label(root, text=" "*1000, font=(FONT, DayFontSize, "bold"), bg=DayBG).place(relx=0.5, rely=0.2, anchor=CENTER)
+Mon = Label(root, text="Monday",    font=(FONT, DayFontSize, "bold"), fg=DayFG, bg=DayBG).place(relx=GridCenter-3*DaySpacing, rely=0.2, anchor=DayAnchor)
+Tue = Label(root, text="Tuesday",   font=(FONT, DayFontSize, "bold"), fg=DayFG, bg=DayBG).place(relx=GridCenter-2*DaySpacing, rely=0.2, anchor=DayAnchor)
+Wed = Label(root, text="Wednesday", font=(FONT, DayFontSize, "bold"), fg=DayFG, bg=DayBG).place(relx=GridCenter-DaySpacing,   rely=0.2, anchor=DayAnchor)
+Thu = Label(root, text="Thursday",  font=(FONT, DayFontSize, "bold"), fg=DayFG, bg=DayBG).place(relx=GridCenter,              rely=0.2, anchor=DayAnchor)
+Fri = Label(root, text="Friday",    font=(FONT, DayFontSize, "bold"), fg=DayFG, bg=DayBG).place(relx=GridCenter+DaySpacing,   rely=0.2, anchor=DayAnchor)
+Sat = Label(root, text="Saturday",  font=(FONT, DayFontSize, "bold"), fg=DayFG, bg=DayBG).place(relx=GridCenter+2*DaySpacing, rely=0.2, anchor=DayAnchor)
+Sun = Label(root, text="Sunday",    font=(FONT, DayFontSize, "bold"), fg=DayFG, bg=DayBG).place(relx=GridCenter+3*DaySpacing, rely=0.2, anchor=DayAnchor)
 
-text = Label(endframe, text="end", font=(FONT, 24, "bold"), fg=FG, bg=BG).pack()
+#Open = Entry(frame2, fg=FG, bg=BG).pack(side=RIGHT)
+
+
+Button(endframe, text="quit", font=(FONT, 24, "bold"), fg=FG, bg="dark red", bd=0, command=quit).pack()
 
 
 
