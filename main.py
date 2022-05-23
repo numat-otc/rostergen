8da5755a441f1c64ced6ddc3bf857c1811f00ef4
numat <numat@stu.otc.school.nz>
2022-05-24 10:59:15 +1200

    testing out day layout

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Libraries
 from tkinter import *
 from tkinter import Canvas, Frame
 import random  # random lib
@@ -12,7 +13,7 @@ FG = ("grey80",)
 FONT = ("Calibri")
 Theme=0
 
-
+# Tkinter setup
 root = Tk()
 frame = Frame(root)
 canvas = Canvas()
@@ -22,29 +23,28 @@ root.title("Roster")  # window title
 root.geometry("1280x720")  # window size
 root.configure(bg=BaseBG)  # background colour
 
+# Fullscreen mode
+FSstate=False
+def ToggleFullscreen(FSstate):
+    root.attributes("-fullscreen", FSstate)
+    FSstate = not FSstate
+
+
 # Frames
 global frame3
-frame1   = Frame(root, bg=BG);                 frame1.pack(side=TOP,   fill=X)
-frame2   = Frame(root, height=50, bg=BG);      frame2.pack(side=TOP,   fill=X)
-frame3   = Frame(root, width=100, height=50, bg="purple"); frame3.pack(side=TOP, fill=BOTH)
+frame1   = Frame(root, height=80, bg=BG);                   frame1.pack(side=TOP,   fill=X)
+frame2   = Frame(root, height=50, bg=BG);                   frame2.pack(side=TOP,   fill=X)
+frame3   = Frame(root, height=50, width=100, bg="purple");  frame3.pack(side=TOP, fill=BOTH)
 frame3Scroll = Scrollbar(frame3).pack(side="right")
 #endframe = Frame(root);                        endframe.pack(side=BOTTOM)
 
 # Title (frame1 top)
-#Label(frame1, text="ROSTER", font=(FONT, 48, "bold"), fg=FG, bg=BG).pack(side=TOP)
-Label(frame1, text="ROSTER", font=(FONT, 48, "bold"), fg=FG, bg=BG).grid(column=1)
-
-# Fullscreen mode
-FSstate=False
-def ToggleFullscreen(state):
-    root.attributes("-fullscreen", state)
-    state = not state
-
+Label(frame1, text="ROSTER", font=("Tahoma", 32, "bold"), fg=FG, bg=BG).place(relx=0.5, rely=0.5, anchor=CENTER)
 
 
 # Window buttons and hotkeys(useful if in fullscreen)
 Button(frame1, text="✖", width=4, font=(FONT, 14), fg=FG, bg="brown4", activeforeground=FG, activebackground="maroon", bd=0, command=quit).place(relx=1, rely=0,anchor=NE)
-Button(frame1, text="☐", width=4, font=(FONT, 14), fg=FG, bg="royalblue3", activeforeground=FG, activebackground="royalblue4", bd=0, command=ToggleFullscreen(state=FSstate)).place(relx=1, rely=0, x=-46,anchor=NE)
+Button(frame1, text="☐", width=4, font=(FONT, 14), fg=FG, bg="purple3", activeforeground=FG, activebackground="purple4", bd=0, command=ToggleFullscreen(FSstate=FSstate)).place(relx=1, rely=0, x=-46,anchor=NE)
 
 #root.bind("<F11>", lambda self: ToggleFullscreen(self, FSstate=FSstate))
 root.bind("<Escape>", quit)
@@ -61,7 +61,7 @@ for day in range(len(DayList)):
 # Peoples (frame3 body mid)
 def frame3add50():
     count =+ 50
-    frame3.config(root, width=100, height=count, bg="purple")
+    frame3.config(height=count)
 
 Button(frame3,text="+50px",command=frame3add50).place(relx=0.5,rely=0.5)
 
@@ -73,7 +73,7 @@ Button(frame3,text="+50px",command=frame3add50).place(relx=0.5,rely=0.5)
 
 
 
-
+#root.after(2000, quit)
 root.mainloop()  # end of tk
 
 
