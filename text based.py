6802de1d5724cee5e070d8a40a1572078cacb430
numat <numat@stu.otc.school.nz>
2022-07-06 15:17:47 +1200

    text based version progress

--- text based.py
+++ text based.py
@@ -1,68 +1,86 @@
+# setup
 import os
 import time
 
 global dayslist
+global daysshortlist
 global everyone
-dayslist = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+dayslist = ["   Monday","  Tuesday","Wednesday"," Thursday","   Friday"," Saturday","   Sunday"]
+daysshortlist = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
 everyone = []
 
-def wait(t):
+def wait(t): # time lib wait function
     time.sleep(t)
 
-def PrintRoster():
+def PrintRoster(): # display names and hours
     print("═" * 24)
     print("Roster:")
     for i in range(len(everyone)):
         print(everyone[i])
     print("═" * 24)
 
-def add():
+def add(): # add someone new and their hours
     while True:
         os.system("cls")
         PrintRoster()
-        print("Type 'Cancel' to go back")
+        print("Type 'Cancel' and ENTER to go back")
         print("\nType a new name and ENTER")
-        inputname = input("|>  ")
-        if inputname.lower() == "cancel":
+        inputname = input("|>  ") # input
+        if inputname.lower() == "cancel": # if canceling go back
             return
-        elif inputname in everyone:
+        elif inputname in everyone: # if person already exists (exact characters)
             print("Person already exists")
             wait(t=1)
             continue
         else:break
-
-    #everyone.append(inputname)
-    #return
+    globals()[f"list{inputname}"] = []
+    everyone.append(inputname)
     while True:
-        print("hours entry tbd")
+        os.system("cls")
+        print("Type 'Cancel' and ENTER to go back")
+        print("Type a number corresponding to a day and ENTER")
+        print("Type nothing and ENTER to finish")
+        print("═" * 24)
+        print(f"{inputname}:")
+        for i in range(len(dayslist)):
+            print(f"{i+1} - {dayslist[i]}:  ")
+            # and hoursssss
+
+        print("═" * 24)
+        inputday = input("|>  ") # input
+        if inputday.lower() == "cancel": # if canceling go back
+            return
+        if inputday == "":
+            pass ### save vars, go back
+        print(globals()[f"list{inputname}"])
+        input()
 
 
 
-
-def remove():
+def remove(): # remove a person and their hours
     os.system("cls")
     PrintRoster()
     pass #tbd
 
-def edit():
+def edit(): # edit a person and their hours
     os.system("cls")
     PrintRoster()
     pass #tbd
 
 
-while True:
-    os.system("cls")
-    PrintRoster()
-    print("\nType 'Add' or 'Remove' or 'Edit' and ENTER")
-    inputoption = input("|>  ")
+while True: # main loop
+    os.system("cls") # clear command prompt screen
+    PrintRoster() # print roster
+    print("\nType 'Add' or 'Remove' or 'Edit' and ENTER") # options
+    inputoption = input("|>  ") # input
 
-    if inputoption.lower() in ["+","add"]:
+    if inputoption.lower() in ["+","add"]: # if add option selected
         add()
 
-    if inputoption.lower() in ["-","remove","delete"]:
+    if inputoption.lower() in ["-","remove","delete"]: # if remove option selected
         remove()
 
-    if inputoption.lower() in [".","edit","change"]:
+    if inputoption.lower() in [".","edit","change"]: # if edit option selected
         edit()
 
 
